374585c9ea341b082d913d553cdf78778543cba8
rakadedy <54363168+rakadedy@users.noreply.github.com>
2019-08-21 20:36:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,10 @@
-<?php?>
+<?php
+	if(isset($_POST['submit'])){
+		if ($_POST['username']=="sesuatu" && $_POST['pass']=="rahasia") {
+			header("Location:login.php");
+		}
+		else{
+			header("Location:index.php");
+		}
+	}
+?>
