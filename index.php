73fcd63ea24633d7c1fe1454a7a02c38a20aa9c0
rakadedy <54363168+rakadedy@users.noreply.github.com>
2019-08-21 20:32:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-
+	if(isset($_POST['submit'])){
+		if ($_POST['username']=="sesuatu" && $_POST['pass']=="rahasia") {
+			header("Location:login.php");
+		}
+		else{
+			header("Location:index.php");
+		}
+	}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Index</title>
+</head>
+<body>
+	<center>
+		<form method="POST" action="#">
+			<h3>Username :</h3>
+			<input type="text" name="username">
+			<h3>Password :</h3>
+			<input type="password" name="pass">
+			<br><br>
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</center>
+</body>
+</html>
