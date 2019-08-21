0749cb8689564812e813297f88dfac95520e625a
Adverga <bagusvernandaap@gmail.com>
2019-08-21 20:41:23 +0700

    update proses + web

--- proses.php
+++ proses.php
@@ -1,1 +1,12 @@
-<?php?>
+<?php
+	session_start();
+	if (!isset($_SESSION['username'])) {
+		header('Location:index.php');
+	}
+	$n = $_GET['nilai-n'];
+	for ($i=1; $i <= $n ; $i++) { 
+		for ($j=1; $j <=$1 ; $j++) { 
+			echo $j;
+		}
+	}	
+?>
--- web.php
+++ web.php
@@ -1,1 +1,21 @@
-<?php?>
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+	header("Location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Akhir</title>
+</head>
+<body>
+	<center>
+		<form action="proses.php" method="GET">
+			Input nilai-n: <br>
+			<input type="text" name="nilai-n"><br>
+			<input type="submit" value="Submit">
+		</form>	
+	</center>
+</body>
+</html>
